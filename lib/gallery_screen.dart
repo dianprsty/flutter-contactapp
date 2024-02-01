7abfffe5396d520ae0b379a8ec41b152e40f9499
dianp <dprasmail@gmail.com>
2024-02-01 08:42:59 +0700

    assets and navigation : home page

---
+++ lib/gallery_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class GalleryScreen extends StatefulWidget {
+  const GalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gallery"),
+      ),
+      body: Center(
+        child: const Text("Gallery"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:contactapp/bottom_navigation.dart';
+import 'package:contactapp/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,11 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const BottomNavigation());
+      theme: ThemeData(fontFamily: "Quicksand"),
+      home: HomeScreen(),
+    );
   }
 }
